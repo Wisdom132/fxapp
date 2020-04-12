85c49b65a1258703cd9ae7bef50e4a4dcf63309e
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-04-12 23:30:32 +0100

    fixed baseurl link

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -10,10 +10,10 @@ interface myData {
 	providedIn: 'root'
 })
 export class AuthService {
-	// readonly rootURL: string = 'https://manny-fxapp.herokuapp.com';
-	readonly rootURL: string = 'http://localhost:5000';
+	readonly rootURL: string = 'https://manny-fxapp.herokuapp.com';
+	// readonly rootURL: string = 'http://localhost:5000';
 
-	constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) { }
 
 	userSignin(data) {
 		return this.http.post(`${this.rootURL}/api/users/login`, data);
